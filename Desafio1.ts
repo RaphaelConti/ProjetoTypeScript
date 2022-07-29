ff63d534d06fcad38640fbca99c1dc1ed0048d70
RaphaelConti <rconti4@gmail.com>
2022-07-29 13:40:05 -0300

    desafio concluido

---
+++ Desafio1.ts
@@ -0,0 +1,6 @@
+let employee: {code: number, name: string} = {
+    code: 5,
+    name: 'Raphael'
+};
+employee.code = 10;
+employee.name = "John";
---
+++ Desafio2.ts
@@ -0,0 +1,34 @@
+let pessoa1:{nome:string,idade:number,profissao:string} = {
+    nome: 'raphael',
+    idade: 30,
+    profissao: 'desenvolvedor'
+};
+pessoa1.nome = "maria";
+pessoa1.idade = 29;
+pessoa1.profissao = "atriz"
+
+let pessoa2:{nome:string,idade:number,profissao:string} = {
+    nome: 'gabriel',
+    idade: 15,
+    profissao: 'estudante'
+};
+pessoa2.nome = "roberto";
+pessoa2.idade = 19;
+pessoa2.profissao = "Padeiro";
+
+let pessoa3:{nome:string,idade:number,profissao:string} = {
+    nome: "laura",
+    idade: 32,
+    profissao: "Atriz"
+};
+pessoa3.idade = 28;
+pessoa3.nome = "Amanda"
+pessoa3.profissao = "vendedora da claro"
+
+
+let pessoa4:{nome:string,idade:number,profissao:string} = {
+    nome: "carlos",
+    idade: 19,
+    profissao: "padeiro"
+}
+pessoa4.profissao = "chefe de cozinha"
